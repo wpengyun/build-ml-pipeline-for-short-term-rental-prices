7d422805d1effb1fd1357deb6d1e355f1617bdc5
wpy <Pengyun_Wang@mckinsey.com>
2022-11-15 14:00:59 +0800

    fix a bug

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,6 +29,8 @@ def go(args):
     ######################
     logger.info("removing outliers")
     df=df[df["price"].between(args.min_price, args.max_price)]
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     logger.info("saving and uploading output artifact")
     with tempfile.NamedTemporaryFile() as fp:
